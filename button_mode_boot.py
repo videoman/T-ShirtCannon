409a2af11287c68aa8ffabd8cf406f1d74fbe7f7
videoman <dave@drstrangelove.net>
2013-12-29 23:07:52 -0600

    Added an include file.

---
+++ button_mode_boot.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+# LEDS
+LED1=2
+LED2=14
+LED3=3
+LED4=15
+LED5=24
+
+# Define our input buttons here
+BUTTON1=27
+BUTTON2=23
+
+# Setup the GPIO stuff.
---
+++ launcher_pins.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+# This file defines the output and input pins for the T-Shirt Launcher
+
+# Define our relay outputs
+# Each output is triggered via a ULN2803 Darlington Driver
+TSHIRT1=4
+TSHIRT2=17
+TSHIRT3=18
+TSHIRT4=22
+
+# Define our LED outputs
+# LED status for each barrel
+LED1=2
+LED2=14
+LED3=3
+LED4=15
+
+# Either for errors or for the championship games- confetti
+LED5=24
+
+# Define our input buttons here
+# BUTTON1 is for Selecting 
+BUTTON1=27
+
+# BUTTON2 is for firing a t-shirt
+BUTTON2=23
+
