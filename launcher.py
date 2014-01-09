d0b4ce8cf131056db493c92de544aaeeedae9c83
videoman <dave@drstrangelove.net>
2014-01-09 17:45:48 -0600

    Updated the code so that it will auto advance to the next barrel.
    Adding a boot button so that we can change wifi modes when the system boots.
    
    So it will either setup an AP, or connect to a wifi network.

--- launcher.py
+++ launcher.py
@@ -5,34 +5,12 @@
 
 # Copywrite David M. N. Bryan, all rights reserved.
 
-import RPi.GPIO as GPIO, time
-
-# Define our relay outputs
-TSHIRT1=4
-TSHIRT2=17
-TSHIRT3=18
-TSHIRT4=22
-
-# Define our LED outputs
-#LED1=2
-#LED2=3
-#LED3=14
-#LED4=15
-#LED5=24
-LED1=2
-LED2=14
-LED3=3
-LED4=15
-LED5=24
-
-# Define our input buttons here
-BUTTON1=27
-BUTTON2=23
-#BUTTON1=23
-#BUTTON2=27
+import RPi.GPIO as GPIO, time 
+from launcher_pins import *
 
 # Setup our GPIO lines
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
 # Output to the Darlington Driver for the 12V sprinkler valves
 GPIO.setup(TSHIRT1, GPIO.OUT, pull_up_down=GPIO.PUD_DOWN)
@@ -48,7 +26,9 @@ GPIO.setup(LED4, GPIO.OUT, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(LED5, GPIO.OUT, pull_up_down=GPIO.PUD_DOWN)
 
 # Setup the Inputs
+# Button one is the selector switch.
 GPIO.setup(BUTTON1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# Button 2 is Launcher PIN
 GPIO.setup(BUTTON2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # Setup a t-shirt shooting function
@@ -59,26 +39,35 @@ def fireshirt(TNUMBER):
 	global valve_sleep_time
 	if (TNUMBER==1):
 		TSHIRTN=TSHIRT1
+		LEDN=LED1
 	elif (TNUMBER==2):
 		TSHIRTN=TSHIRT2
+		LEDN=LED2
+		# Valve 2 is a Rain Bird, and requires more time to bleed the air
 		valve_sleep_time=.18
 	elif (TNUMBER==3):
 		TSHIRTN=TSHIRT3
+		LEDN=LED3
 	elif (TNUMBER==4):
 		TSHIRTN=TSHIRT4
+		LEDN=LED4
         else:
 		TSHIRTN=0
 
 	if(ready==1): 
 		print "Turning on valve for", valve_sleep_time, "second using pin: ", TSHIRTN
 		blink_led(TNUMBER, 3, .1)
-
+		
+		# Turn on the Valve
         	GPIO.output(TSHIRTN, True)
         	time.sleep(valve_sleep_time)
+		#Turn off the valve
         	GPIO.output(TSHIRTN, False)
     		F_TIME=time.time()
 
-		blink_led(TNUMBER, 3, .5)
+		blink_led(TNUMBER, 3, .2)
+		# Turn the LED off plz
+		GPIO.output(LEDN, False)
 	else:
 		#print "Fire command requested for", THSIRTN, " but we're not ready..."
 		print('Fire button detected, but time out not yet passed: Firetime: {0} Pushtime: {1}'.format(F_TIME,time.time()))
@@ -131,63 +120,71 @@ def blink_led(LED_NUM, REPS, DELAY):
 		time.sleep(DELAY)
 	
 	GPIO.output(LEDN, True)
-		
 
 LMODE=1
 light_led(LMODE)
-MAX_MODES=4
+MAX_MODES=5
+
+def increment_lmode():
+	global LMODE
+	# day after day, add it up!
+	LMODE += 1
+	if(LMODE<=0):
+		LMODE=1
+	elif(LMODE>=MAX_MODES):
+		LMODE=1
+    	print('Setting launch mode to %s'%LMODE)
+    	light_led(LMODE)
 
 # These answer button pushes.
 def my_callback_sel(BUTTON1):
+	increment_lmode()
+
+'''    global LMODE
+    # We have a momentary switch that we want to increment each time it's pressed, and light up LED X.
+    LMODE += 1
+    if(LMODE<=0):
+	LMODE=1
+    elif(LMODE>=MAX_MODES):
+	LMODE=1
+    print('Setting launch mode to %s'%LMODE)
+    light_led(LMODE)
+'''
+
+'''def button_select():
     global LMODE
     # We have a momentary switch that we want to increment each time it's pressed, and light up LED X.
     if(LMODE<=0):
-	LMODE=0
-    elif(LMODE==MAX_MODES):
-	LMODE=0
+	LMODE=1
+    elif(LMODE>=MAX_MODES):
+	LMODE=1
     LMODE += 1
     print('Setting launch mode to %s'%LMODE)
-    light_led(LMODE)
+    light_led(LMODE) 
+'''
 
-BUTTON2_P=0
-def my_callback_fire(BUTTON2):
-	global BUTTON2_P
-	BUTTON2_P += 1
-	if(BUTTON2_P<=1):
-		F_TIME2 = F_TIME + 4
-		ptime=time.time()
-		if(F_TIME2<=ptime):
-			ready=1
-			print('Fireing using launch mode %s'%LMODE)
-			BUTTON_P=0
-			fireshirt(LMODE)
-		else:
-			print('Waiting for delay_time to timeout... F_TIME: {0} Current: {1}' .format(F_TIME,ptime))
-			ready=0
-	else:
-		print('Second push detected...')
-		BUTTON2_P=0
 
+# This is an Event on push button.  It works well- but static can cause it to be pushed.
 GPIO.add_event_detect(BUTTON1, GPIO.FALLING, callback=my_callback_sel, bouncetime=200)
-#GPIO.add_event_detect(BUTTON2, GPIO.FALLING, callback=my_callback_fire, bouncetime=1000)
+# GPIO.add_event_detect(BUTTON2, GPIO.FALLING, callback=my_callback_fire, bouncetime=1000)
 
 F_TIME=time.time()
 ready=1
 
 while True:
    global ready
-   time.sleep(.025)
+   time.sleep(.0005)
    if(ready==0):
 	# Blink the status LED.
 	blink_led(5, 5, .5)
 
    # Wait to see if the Fire button is being held down... 
    if(GPIO.input(BUTTON2) == 0):
-	  time.sleep(.30)
+	  time.sleep(.25)
 	  if(GPIO.input(BUTTON2) == 0):
-	 	time.sleep(.30)
+	 	time.sleep(.25)
 		if(GPIO.input(BUTTON2) == 0):
 			#my_callback_fire
 			fireshirt(LMODE)
-			time.sleep(1)
-		
+			increment_lmode()
+			time.sleep(0.5)
