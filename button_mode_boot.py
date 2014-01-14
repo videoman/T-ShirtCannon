732876e03985772d755ac79a6586d123da3197ed
videoman <dave@drstrangelove.net>
2014-01-14 11:54:04 -0600

    Updated LICENSE, and button mode script.  More to come later..

--- button_mode_boot.py
+++ button_mode_boot.py
@@ -1,14 +1,31 @@
 #!/usr/bin/python
+#
+# David M. N. Bryan
+# Attribution-NonCommercial-ShareAlike 4.0 International
+#
 
-# LEDS
-LED1=2
-LED2=14
-LED3=3
-LED4=15
-LED5=24
+# This file will be used to update and change the wifi setting before launching the 
+# T-Shirt cannon application
 
-# Define our input buttons here
-BUTTON1=27
-BUTTON2=23
+import RPi.GPIO as GPIO, time
+from launcher_pins import *
 
-# Setup the GPIO stuff.
+# Setup our GPIO lines
+GPIO.setmode(GPIO.BCM)
+# Turn off warnings...
+GPIO.setwarnings(False)
+
+# Setup the Inputs
+# Button one is the selector switch.
+GPIO.setup(BUTTON1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# Button 2 is Launcher PIN
+GPIO.setup(BUTTON2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# Wait to see if the select button is being held down... 
+if(GPIO.input(BUTTON1) == 0):
+   time.sleep(.5)
+   if(GPIO.input(BUTTON1) == 0):
+      time.sleep(.5)
+      if(GPIO.input(BUTTON1) == 0):
+         # Do some wifi changes!
+         time.sleep(0.5)
